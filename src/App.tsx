397fed8a952a679192abcdbcfc1ff7641e57a9a2
Michał Taberski <m.taberski@gmail.com>
2024-12-19 14:57:15 +0100

    basic table

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,148 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import { DataTable } from "./components/data-table";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "./components/ui/tooltip";
+
+type Game = {
+  name: string;
+  result: [number, number];
+  status: "win" | "loss" | "ongoing-win" | "ongoing-loss";
+};
+
+const fetchGames = async (): Promise<Game[]> => {
+  // axios.get("http://localhost:3000/api/games");
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  return [
+    {
+      name: "Roma -> Juve",
+      result: [1, 0],
+      status: "ongoing-win",
+    },
+    {
+      name: "Milan -> Inter",
+      result: [0, 2],
+      status: "ongoing-loss",
+    },
+    {
+      name: "Napoli -> Lazio",
+      result: [3, 1],
+      status: "win",
+    },
+    {
+      name: "Atalanta -> Fiorentina",
+      result: [1, 2],
+      status: "loss",
+    },
+  ];
+};
+
+type FetchResult<T> = {
+  data: T | null;
+  loading: boolean;
+  error: string | null;
+};
+
+type FetchOptions = {
+  refetch?: number;
+};
+
+const useFetch = <T,>(
+  fn: () => Promise<T>,
+  options: FetchOptions = {}
+): FetchResult<T> => {
+  const [data, setData] = useState<T | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+  const refetch = options.refetch ?? null;
+
+  useEffect(() => {
+    fn()
+      .then(setData)
+      .catch(setError)
+      .finally(() => setLoading(false));
+  }, [fn]);
+
+  useEffect(() => {
+    if (refetch) {
+      setInterval(() => {
+        fn()
+          .then(setData)
+          .catch(setError)
+          .finally(() => setLoading(false));
+      }, refetch);
+    }
+  }, [refetch]);
+
+  return { data, loading, error };
+};
+
+const STATUS_COLORS = {
+  win: "bg-green-600",
+  loss: "bg-red-600",
+  "ongoing-win": "bg-green-300",
+  "ongoing-loss": "bg-red-300",
+};
+
+const GameStatusBadge = ({ status }: { status: Game["status"] }) => {
+  return (
+    <Tooltip>
+      <TooltipTrigger>
+        <div
+          className={`w-4 h-4 rounded-full ml-2 inline-block ${STATUS_COLORS[status]}`}
+        />
+      </TooltipTrigger>
+      <TooltipContent>
+        {status === "win"
+          ? "Team 1 won"
+          : status === "loss"
+          ? "Team 2 won"
+          : "Game is ongoing"}
+      </TooltipContent>
+    </Tooltip>
+  );
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { data, loading } = useFetch(fetchGames);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="bg-gray-100 flex justify-center items-center h-screen py-4">
+      <div className="p-4 m-4 bg-white border flex-1 flex-col flex h-full rounded-sm">
+        {loading ? (
+          <div>Loading...</div>
+        ) : (
+          <DataTable
+            data={data ?? []}
+            columns={[
+              {
+                accessorKey: "name",
+                header: "Game 1",
+              },
+              {
+                accessorKey: "result",
+                header: "Result",
+                cell: ({ row }) => {
+                  const result = row.original.result;
+                  const status = row.original.status;
+                  return (
+                    <div className="flex items-center">
+                      <pre>
+                        {result[0]} - {result[1]}
+                      </pre>
+                      <GameStatusBadge status={status} />
+                    </div>
+                  );
+                },
+              },
+            ]}
+          />
+        )}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
